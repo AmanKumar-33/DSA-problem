b85945a78bd1890ed4f43f970c8f73c6819e697a
AmanKumar-33 <amansingh9876545@gmail.com>
2023-12-09 00:13:00 +0530

    added a new linkedList question

---
+++ LinkedList/LList1.cpp
@@ -0,0 +1,22 @@
+// Write a program for linked list implementation.
+#include <iostream>
+using namespace std;
+// a structure for node in linked list
+struct Node
+{
+    int data;
+    Node *next;
+}Node;
+//  Node* first = NULL;
+// *first = NULL;
+// Node* temp = (Node*)malloc(sizeof(Node));
+int main(){
+    int n =0;
+     Node* first = NULL;
+    cout<<"Enter the number of nodes you want to create: ";
+   
+    cin>>n;
+
+
+}
+
